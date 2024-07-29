ea387bc6cb90aaae6165d911bf101f01b7f36e6a
Dayan Yamin <yamindayan@gmail.com>
2024-07-29 16:06:43 -0400

    delete unnused page

--- src/app/home/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-50 font-black">
-      <h1 className="font-black">Bienvenidos a Car2Car</h1>
-    </main>
-  );
-}
