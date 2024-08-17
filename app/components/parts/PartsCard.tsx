38a90eb727ef372b35b7c68f13d2742f67aec525
Dayan Yamin <yamindayan@gmail.com>
2024-08-17 15:29:59 -0400

    fix styles

--- app/components/parts/PartsCard.tsx
+++ app/components/parts/PartsCard.tsx
@@ -9,13 +9,13 @@ interface CardProps {
 
 const Card = ({ part }: CardProps) => {
   return (
-    <div className="max-w-60 rounded overflow-hidden shadow-lg m-4">
+    <div className="w-full rounded overflow-hidden shadow-lg m-4">
       <Image
         className="h-52 max-h-52"
         src={part.imageUrl || DEFAULT_PART_IMAGE}
         alt={part.name}
-        width="300"
-        height="300"
+        width="600"
+        height="600"
       />
       <div className="px-6 py-6 h-30">
         <div className="text-black font-bold text-xl mb-2 truncate">
--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,12 @@ export default function Home() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-6 w-full ">
       {parts.map((part) => (
-        <Link key={part._id} href={`/parts/${part._id}`} passHref>
+        <Link
+          key={part._id}
+          href={`/parts/${part._id}`}
+          className="w-full"
+          passHref
+        >
           <CardParts key={part._id} part={part} />
         </Link>
       ))}
