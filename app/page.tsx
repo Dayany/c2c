da43a6fd4e9740ce4f35461cde0530221b231480
Dayan <yamindayan@gmail.com>
2024-08-28 20:35:57 -0400

    Main page now backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,24 @@
-"use client";
 import React from "react";
-import { useEffect, useState } from "react";
 import CardParts from "@/components/parts/PartsCard";
 import { Part } from "@/types";
 import Link from "next/link";
 
-export default function Home() {
-  const [parts, setParts] = useState<Part[]>([]);
+async function fetchData(): Promise<Part[] | null> {
+  const baseUrl: string | undefined = process.env.NEXT_PUBLIC_BASE_URL;
+  const response = await fetch(`${baseUrl}/api/parts`);
 
-  useEffect((): void => {
-    const fetchProducts = async (): Promise<void> => {
-      const response = await fetch("/api/parts");
-      const data: Part[] = await response.json();
-      setParts(data);
-    };
+  if (!response.ok) {
+    return null;
+  }
+  return response.json();
+}
 
-    fetchProducts();
-  }, []);
+export default async function Home() {
+  const parts: Part[] | null = await fetchData();
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-6 w-full ">
-      {parts.map((part: Part) => (
+      {parts?.map((part: Part) => (
         <Link
           key={part._id}
           href={`/parts/${part._id}`}
